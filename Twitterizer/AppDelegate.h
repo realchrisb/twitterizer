30752389ea093c9401981a63091cb9a839b52db8
Christopher Benavides <realchrisb@icloud.com>
2016-05-25 14:16:59 -0500

    Initial Commit

---
+++ Twitterizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitterizer
+//
+//  Created by Christopher Benavides on 5/25/16.
+//  Copyright © 2016 Christopher Benavides. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitterizer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Twitterizer
+//
+//  Created by Christopher Benavides on 5/25/16.
+//  Copyright © 2016 Christopher Benavides. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
