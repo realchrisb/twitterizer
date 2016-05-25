4c5374bfea28fc0824f27e6c67ccc11852d67040
Christopher Benavides <chrisbreezy@Christophers-MacBook-Pro.local>
2016-05-25 15:19:59 -0500

    TwitterizerEdit

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -9,11 +9,24 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
 @implementation ViewController
+- (IBAction)twitterize:(id)sender {
+    NSString *vowels = self.textView.text;
+    NSMutableString *noVowel = [[NSMutableString alloc] init];
+    
+    if ([char character [vowels characterAtUIndex:vowels.length]]) {
+        <#statements#>
+        
+    } else {
+        <#statements#>
+    }
 
+    
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
